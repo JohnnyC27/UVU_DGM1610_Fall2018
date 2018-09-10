c4004cfbed0e7c532d7c8534c808f5c3c261addc
Johnny Cordova <johnnycordova40@gmail.com>
2018-09-10 17:11:38 -0600

    created first script character moves

---
+++ DGM_1610_Game/Assets/scrpits/CharacterMove.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMove : MonoBehaviour {	
+	
+	public int MoveSpeed;
+	public float JumpHeight = 15;
+
+	// Use this for initialization
+	void Start () {
+		print("Hello!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
