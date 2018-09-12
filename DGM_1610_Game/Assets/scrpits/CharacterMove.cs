118d385dc926591adea7f963049444afcf08e2cb
Johnny Cordova <johnnycordova40@gmail.com>
2018-09-12 17:16:39 -0600

    created character move script

--- DGM_1610_Game/Assets/scrpits/CharacterMove.cs
+++ DGM_1610_Game/Assets/scrpits/CharacterMove.cs
@@ -1,19 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class CharacterMove : MonoBehaviour {	
+public class CharacterMove : MonoBehaviour {
 	
+	// Player Movement Variables 
 	public int MoveSpeed;
-	public float JumpHeight = 15;
+	public float JumpHeight;
+
+	// Player grounded variables 
+	public Transform GroundCheck;
+	public flaot GroundCheckRadius;
+	public LayerMask WhatIsGround;
+	private bool Grounded;
 
 	// Use this for initialization
 	void Start () {
 		print("Hello!");
 	}
 	
+	
+	void FixedUpdate () {
+		Grounded = Physics2D.OverlapCircle (GroundCheck.position, GroundCheckRadius, WhatIsGround);
+	}
+	
 	// Update is called once per frame
 	void Update () {
 		
+		//This code makes the character jump
+		if(Input.GetKeyDown (KeyCode.Space)&& Grounded){
+			Jump();
+		}
+
+		
+
+
+
+
+
+
+
+
+
+
+
+		Public Void Jump(){
+			GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, JumpHeight);
+		}
+
 	}
-}
